83a08758a8130b966ce6962f9ae5d675eab9b46b
huangjun.emc2@gmail.com <huangjun.emc2@gmail.com>
2024-01-24 16:46:58 +0800

    complete 605

---
+++ src/test/java/com/leetcode/CanPlaceFlowersTest.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CanPlaceFlowersTest {
+    @Test
+    public void test() {
+        CanPlaceFlowers solution = new CanPlaceFlowers();
+        Assert.assertTrue(solution.canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
+        Assert.assertFalse(solution.canPlaceFlowers(new int[]{1,0,0,0,1}, 2));
+    }
+}
