0ca89f00fe70d0b047062c469d8bcb7a171a312b
huangjun.emc2@gmail.com <huangjun.emc2@gmail.com>
2024-02-01 14:39:40 +0800

    use better int hash

--- src/main/java/com/leetcode/m2352_equal_pairs/Solution.java
+++ src/main/java/com/leetcode/m2352_equal_pairs/Solution.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+    private int intHash(int val) {
+        long v = val;
+        v *= 2654435761L;
+        return (int)v >> 32;
+    }
+
     public int equalPairs(int[][] grid) {
         Map<Integer, Integer> rowHashes = new HashMap<>();
         int count = 0;
@@ -11,7 +17,7 @@ class Solution {
         for (int i = 0; i < grid.length; i++) {
             int hash = 1;
             for (int j = 0; j < grid[i].length; j++) {
-                hash = hash * 127 + grid[i][j];
+                hash = hash * 127 + intHash(grid[i][j]);
             }
             rowHashes.put(hash, rowHashes.getOrDefault(hash, 0) + 1);
         }
@@ -19,11 +25,12 @@ class Solution {
         for (int j = 0; j < grid[0].length; j++) {
             int hash = 1;
             for (int i = 0; i < grid.length; i++) {
-                hash = hash * 127 + grid[i][j];
+                hash = hash * 127 + intHash(grid[i][j]);
             }
             count += rowHashes.getOrDefault(hash, 0);
         }
 
         return count;
     }
+
 }
