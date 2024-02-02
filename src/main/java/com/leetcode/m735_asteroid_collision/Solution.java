7e2f5d76c56b8f48451d18b1eb0f81eb7ba3f9c2
John Huang <huangjun.emc2@gmail.com>
2024-02-02 14:21:14 +0800

    commit 735

--- src/main/java/com/leetcode/m735_asteroid_collision/Solution.java
+++ src/main/java/com/leetcode/m735_asteroid_collision/Solution.java
@@ -1,32 +1,57 @@
 package com.leetcode.m735_asteroid_collision;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < asteroids.length; i ++) {
+        int top = 0;
+        for (int cur : asteroids) {
             // crash until ...
-            Integer cur = asteroids[i];
-            while (cur != null && cur < 0 && !st.isEmpty() && st.peek() > 0) {
-                int top = st.peek();
+            while (cur < 0 && top > 0 && asteroids[top - 1] > 0) {
+                int topVal = Math.abs(asteroids[top - 1]);
+                int curVal = Math.abs(cur);
 
-                if (cur + top <= 0) {
+                if (topVal <= curVal) {
+                    top --;
+                }
+
+                if (curVal <= topVal) {
+                    cur = 0;
+                }
+            }
+
+            // push if still alive
+            if (cur != 0) {
+                asteroids[top ++] = cur;
+            }
+        }
+
+        return Arrays.copyOf(asteroids, top);
+    }
+
+    public int[] asteroidCollision2(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for (Integer cur : asteroids) {
+            // crash until ...
+            while (cur != null && cur < 0 && !st.isEmpty() && st.peek() > 0) {
+                int topVal = Math.abs(st.peek());
+                int curVal = Math.abs(cur);
+
+                if (topVal <= curVal) {
                     st.pop();
                 }
 
-                if (cur + top >= 0) {
+                if (curVal <= topVal) {
                     cur = null;
                 }
             }
 
-            // push if not crashed
+            // push if still alive
             if (cur != null) {
                 st.push(cur);
             }
         }
-
-        
 
         int[] result = new int[st.size()];
         for (int i = 0; i < st.size(); i ++) {
--- src/test/java/com/leetcode/m735_asteroid_collision/SolutionTest.java
+++ src/test/java/com/leetcode/m735_asteroid_collision/SolutionTest.java
@@ -11,4 +11,12 @@ public class SolutionTest {
         Assert.assertArrayEquals(new int[]{}, solution.asteroidCollision(new int[]{8,-8}));
         Assert.assertArrayEquals(new int[]{10}, solution.asteroidCollision(new int[]{10,2,-5}));
     }
+
+    @Test
+    public void asteroidCollisionTest2() {
+        Solution solution = new Solution();
+        Assert.assertArrayEquals(new int[]{5,10}, solution.asteroidCollision2(new int[]{5,10,-5}));
+        Assert.assertArrayEquals(new int[]{}, solution.asteroidCollision2(new int[]{8,-8}));
+        Assert.assertArrayEquals(new int[]{10}, solution.asteroidCollision2(new int[]{10,2,-5}));
+    }
 }
