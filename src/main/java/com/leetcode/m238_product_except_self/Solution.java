a32d15808f9947d63136c26f2c671cd998a376a4
huangjun.emc2@gmail.com <huangjun.emc2@gmail.com>
2024-11-07 16:11:17 +0800

    feat: m238

--- src/main/java/com/leetcode/m238_product_except_self/Solution.java
+++ src/main/java/com/leetcode/m238_product_except_self/Solution.java
@@ -22,4 +22,23 @@ public class Solution {
 
         return result;
     }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+
+        int prefix = 1;
+        for (int i = 1; i < nums.length; i ++) {
+            prefix *= nums[i - 1];
+            ans[i] = prefix;
+        }
+
+        int suffix = 1;
+        for (int i = nums.length - 2; i >= 0; i --) {
+            suffix *= nums[i + 1];
+            ans[i] *= suffix;
+        }
+
+        return ans;
+    }
 }
--- src/test/java/com/leetcode/m238_product_except_self/SolutionTest.java
+++ src/test/java/com/leetcode/m238_product_except_self/SolutionTest.java
@@ -10,4 +10,11 @@ public class SolutionTest {
         Assert.assertArrayEquals(new int[]{24, 12, 8, 6}, solution.productExceptSelf(new int[]{1, 2, 3, 4}));
         Assert.assertArrayEquals(new int[]{0, 0, 9, 0, 0}, solution.productExceptSelf(new int[]{-1, 1, 0, -3, 3}));
     }
+
+    @Test
+    public void testProductExceptSelf2() {
+        Solution solution = new Solution();
+        Assert.assertArrayEquals(new int[]{24, 12, 8, 6}, solution.productExceptSelf2(new int[]{1, 2, 3, 4}));
+        Assert.assertArrayEquals(new int[]{0, 0, 9, 0, 0}, solution.productExceptSelf2(new int[]{-1, 1, 0, -3, 3}));
+    }
 }
